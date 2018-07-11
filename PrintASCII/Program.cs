c8f484e9013f0df7095904a12b02371a92c15d31
sekhar <legnint>
2018-07-12 00:05:01 +0530

    Added sourse code

---
+++ PrintASCII/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrintASCII
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ASCII a = new ASCII ();
+
+			a.PrintAllAscii (180);
+
+
+		}
+	}
+
+	public class ASCII
+	{
+		public void PrintAllAscii(int limit)
+		{
+			for (int i = 0; i <= limit; i++) {
+				Console.WriteLine ("{0} --> {1}", i, (char)i);
+			}
+		}
+
+	}
+
+}
